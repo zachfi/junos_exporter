cecbce50d75e4c615f985a59498baebbe6f62f56
Zach Leslie <xaque208@gmail.com>
2019-03-24 13:25:05 -0700

    Add health endpoint

--- exporter/server.go
+++ exporter/server.go
@@ -1,15 +1,32 @@
 package exporter
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type HealthStatus struct{}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+
+	health := &HealthStatus{}
+
+	bytes, err := json.MarshalIndent(health, "", "\t")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(bytes)
+}
+
 func StartMetricsServer(bindAddr string) {
 	d := http.NewServeMux()
 	d.Handle("/metrics", promhttp.Handler())
+	d.HandleFunc("/status/check", statusHandler)
 
 	err := http.ListenAndServe(bindAddr, d)
 	if err != nil {
